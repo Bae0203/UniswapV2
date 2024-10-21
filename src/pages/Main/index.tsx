b88602714580c2095c40fa440b67f012e6b62432
bae0203 <gmbae06@dgsw.hs.kr>
2024-10-22 00:13:25 +0900

    fix : modal

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import Alert from "../../components/Alert";
+import Calculate from "../../components/Calculate";
+import Modal from "../../components/Modal";
+import useModal from "../../util/hooks/useModal";
 import * as S from "./style";
 
 const Main = () => {
   const [activeAlert, setActiveAlert] = useState<boolean>(false);
+  // const { activeModal } = useModal(); // jotai로 하기
 
   return (
     <>
       {activeAlert && <Alert setActive={setActiveAlert} />}
+      {true && <Modal />}
       <S.MainForm>
         <S.Head>
           <S.Title>스왑</S.Title>
@@ -22,6 +27,7 @@ const Main = () => {
         >
           ddd
         </button> */}
+        <Calculate />
       </S.MainForm>
     </>
   );
