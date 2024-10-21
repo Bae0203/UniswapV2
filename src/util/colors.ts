7be8acc0778bc54856dd6986a940ee03faadfd87
bae0203 <gmbae06@dgsw.hs.kr>
2024-10-21 18:49:16 +0900

    add : color

---
+++ src/util/colors.ts
@@ -0,0 +1,12 @@
+const fontColor = {
+  normal: "#fff",
+  active: "#959595",
+  etc: "#8F95A9",
+} as const;
+
+const PersonalColor = {
+  personal10: "#41434F",
+  personal50: "#2D2F36",
+  personal100: "#191B1F",
+  etc: "#3B6CE2",
+} as const;
