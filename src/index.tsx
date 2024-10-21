07a62eef5cb8ebe06a8703bdc57c74cad9b21389
bae0203 <gmbae06@dgsw.hs.kr>
2024-10-21 19:21:09 +0900

    add : colors(Theme), style Init

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Main from "./pages/Main";
+import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { fontColor, PersonalColor } from "./util/colors";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider
+      theme={{ font: { ...fontColor }, personal: { ...PersonalColor } }}
+    >
+      <Main />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/util/colors.ts
+++ src/util/colors.ts
@@ -1,10 +1,10 @@
-const fontColor = {
+export const fontColor = {
   normal: "#fff",
   active: "#959595",
   etc: "#8F95A9",
 } as const;
 
-const PersonalColor = {
+export const PersonalColor = {
   personal10: "#41434F",
   personal50: "#2D2F36",
   personal100: "#191B1F",
