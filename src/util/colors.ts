efd43cf8eaf7ccf920002e032e1a79fda5352db8
bae0203 <gmbae06@dgsw.hs.kr>
2024-10-21 19:56:53 +0900

    fix : color theme 수정

--- src/util/colors.ts
+++ src/util/colors.ts
@@ -2,11 +2,13 @@ export const fontColor = {
   normal: "#fff",
   active: "#959595",
   etc: "#8F95A9",
+  disabled: "#6F7075",
 } as const;
 
 export const PersonalColor = {
   personal10: "#41434F",
   personal50: "#2D2F36",
+  personal70: "#222429",
   personal100: "#191B1F",
   etc: "#3B6CE2",
 } as const;
