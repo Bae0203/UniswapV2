cd2c9f81de3478b04de5ea11120b47e577c86934
bae0203 <gmbae06@dgsw.hs.kr>
2024-10-21 20:58:29 +0900

    add : 계산 커스텀훅 제작

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,7 +3,7 @@ import Alert from "../../components/Alert";
 import * as S from "./style";
 
 const Main = () => {
-  const [activeAlert, setActiveAlert] = useState<boolean>(true);
+  const [activeAlert, setActiveAlert] = useState<boolean>(false);
 
   return (
     <>
@@ -13,6 +13,15 @@ const Main = () => {
           <S.Title>스왑</S.Title>
           <S.SettingBtn onClick={() => setActiveAlert(true)} />
         </S.Head>
+        {/* <button
+          onClick={() => {
+            let a = 12332.03939303;
+            let b: string = a.toString();
+            console.log(b.slice(0, 10));
+          }}
+        >
+          ddd
+        </button> */}
       </S.MainForm>
     </>
   );
---
+++ src/util/hooks/useCalculate.ts
@@ -0,0 +1,34 @@
+import { ChangeEvent, useState } from "react";
+
+interface ICalculateProps {
+  value: string | number;
+  perPrice: string | number;
+}
+
+const useCalculate = () => {
+  const [token1, setToken1] = useState<number>();
+  const [token2, setToken2] = useState<number>();
+
+  const ChangeEvent = (index: number, event: ChangeEvent<HTMLInputElement>) => {
+    if (index === 1) {
+      setToken1(Number(event.target.value));
+      if (token1 == undefined) setToken2(undefined);
+      else setToken2(calculateToken({ value: token1, perPrice: 1.2 }));
+    } else {
+      setToken2(Number(event.target.value));
+      if (token2 == undefined) setToken1(undefined);
+      else setToken1(calculateToken({ value: token2, perPrice: 1.2 }));
+    }
+  };
+
+  const calculateToken = ({ value, perPrice }: ICalculateProps) => {
+    let price: number = Number(value) * Number(perPrice);
+    if (price.toString().replace(".", "").length > 10)
+      price = Number(price.toString().slice(0.1));
+    return price;
+  };
+
+  return { token1, token2, calculateToken, ChangeEvent };
+};
+
+export default useCalculate;
