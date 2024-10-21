b3ffaa646bea3b0c25dddee540d028029f27b249
bae0203 <gmbae06@dgsw.hs.kr>
2024-10-21 20:20:01 +0900

    fix : activebutton hover 추가

--- src/components/common/Button/style.ts
+++ src/components/common/Button/style.ts
@@ -3,6 +3,10 @@ import styled, { css } from "styled-components";
 const activeButtonStyle = css`
   color: ${({ theme }) => theme.font.normal};
   background-color: ${({ theme }) => theme.personal.etc};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.personal.etcHover};
+  }
 `;
 
 const disableButtonStyle = css`
@@ -13,10 +17,11 @@ const disableButtonStyle = css`
 export const Button = styled.button<{ isActive: boolean }>`
   ${({ isActive }) => (isActive ? activeButtonStyle : disableButtonStyle)}
   width: 100%;
-  padding: 1rem;
-  font-size: 1rem;
+  min-width: 100px;
+  padding: 1rem 0rem;
+  font-size: 14px;
   text-align: center;
   border: none;
   box-shadow: none;
-  border-radius: 10px;
+  border-radius: 14px;
 `;
