45b631f93bf8afe42f4cf927d4abbed54bb876e6
bae0203 <gmbae06@dgsw.hs.kr>
2024-10-22 15:30:08 +0900

    add : 서버 연결

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,18 +1,45 @@
 import { useAtom } from "jotai";
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import Alert from "../../components/Alert";
 import Calculate from "../../components/Calculate";
 import Modal from "../../components/Modal";
 import { isAlertActiveAtom } from "../../store/ActiveState";
+import { TokenId, Keys } from "../../util/constant/token";
+import { customAxios } from "../../util/customAxios";
+import useCalculate from "../../util/hooks/useCalculate";
 import useModal from "../../util/hooks/useModal";
+import useTokenList from "../../util/hooks/useTokenList";
 import * as S from "./style";
 
 const Main = () => {
   const [activeAlert, setActiveAlert] = useAtom<boolean>(isAlertActiveAtom);
-  const { activeModal } = useModal(); // jotai로 하기
+  const { activeModal } = useModal();
+  const { dollarQuote, setDollarQuote } = useCalculate();
+  const { activeToken } = useTokenList();
+
+  const getQuote = async (name: Keys, idx: number) => {
+    await customAxios
+      .get(`&ids=${TokenId[name].id}`)
+      .then((e) => {
+        let cp: number[] = [...dollarQuote];
+        cp[idx] = e.data[TokenId[name].id].usd;
+        setDollarQuote([...cp]);
+        console.log(e.data[TokenId[name].id].usd, idx, cp);
+      })
+      .catch((e) => console.log("error", e));
+  };
+
+  useEffect(() => {
+    if (activeToken[0]) {
+      activeToken.map((name, idx) => {
+        // getQuote(name, idx);
+      });
+    }
+  }, [activeToken]);
 
   return (
     <>
+      {/* <button onClick={() => console.log(dollarQuote)}>dd</button> */}
       {activeAlert && <Alert setActive={setActiveAlert} />}
       {activeModal && <Modal />}
       <S.MainForm>
