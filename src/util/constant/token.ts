7358bbb3bed753bb54cdd41c5d8edbc02ab2ba7c
bae0203 <gmbae06@dgsw.hs.kr>
2024-10-22 10:35:09 +0900

    fix: token 관련 정보 수정

--- src/util/constant/token.ts
+++ src/util/constant/token.ts
@@ -1,4 +1,10 @@
-const TokenId = {
+export interface ITokenInfo {
+  name: string;
+  id: string;
+  amount: string;
+}
+
+export const TokenId = {
   ETH: { id: "ethereum" },
   USDT: { id: "tether" },
   USDC: { id: "usd-coin" },
@@ -10,3 +16,16 @@ const TokenId = {
   CRV: { id: "curve-dao-token" },
   ENS: { id: "ethereum-name-service" },
 } as const;
+
+export const TokenInfo = [
+  { name: "ETH", id: "ethereum", amount: "0" },
+  { name: "USDT", id: "tether", amount: "0" },
+  { name: "USDC", id: "usd-coin", amount: "0" },
+  { name: "DAI", id: "dai", amount: "0" },
+  { name: "AAVE", id: "aave", amount: "0" },
+  { name: "WBTC", id: "bitcoin", amount: "0" },
+  { name: "AXS", id: "axie-infinity", amount: "0" },
+  { name: "COMP", id: "compound-coin", amount: "0" },
+  { name: "CRV", id: "curve-dao-token", amount: "0" },
+  { name: "ENS", id: "ethereum-name-service", amount: "0" },
+] as const;
